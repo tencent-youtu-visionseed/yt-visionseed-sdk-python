5e8373f8c2856fbc0385360b3255967907965cf6
chenliang <chenliang@tencent.com>
2019-08-16 16:23:15 +0800

    [ADD] push v1.1.0 to PyPI

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="visionseed",
-    version="1.0.0",
+    version="1.1.0",
     description="python-sdk for VisionSeed - a camera module with AI ability",
     long_description=README,
     long_description_content_type="text/markdown",
